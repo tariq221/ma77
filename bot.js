fb477a80e761664ef5ef967a66e33b39336118ac
NerLOR#0001 <36960271+mraco@users.noreply.github.com>
2018-12-22 22:28:14 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ LET'S GO!
 client.on('ready', function(){
     client.user.setStatus("dnd");
     var ms = 10000 ;
-    var setGame = [`✪ [ Ms-Squad ] ✪`,`-play`];
+    var setGame = [`-play`];
     var i = -1;
     var j = 0;
     setInterval(function (){
