c8f912989fb3a815ad0afb99ffdda8217844cfc9
tariq221 <46507910+tariq221@users.noreply.github.com>
2019-01-10 03:37:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,15 +51,15 @@ client.on('ready', function(){
 client.on('ready', function(){
  console.log(`Logged in as ${client.user.tag}!`);
     
-   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
-   var s = ['483055660209012736','480169573530861578','483055655800930315'];
+   // var s = ['489342754887827487','489342754887827487','489342754887827487','489342754887827487','489342754887827487'];
+   var s = ['489342754887827487','489342754887827487','489342754887827487'];
     setInterval(function (){
     client.user.setPresence({
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
     name: 'I AM BAD',
-    application_id: '477187715658547201',
+    application_id: '489342754887827487',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
   
